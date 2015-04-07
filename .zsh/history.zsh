55b5811ffeaf532be50d610403c5e08be61cd714
Naoto Mawatari <github.mawatari@gmail.com>
2015-04-07 19:03:17 +0900

    zshの履歴検索をpecoに切り替えた

--- .zsh/history.zsh
+++ .zsh/history.zsh
@@ -25,8 +25,18 @@ setopt share_history
 setopt hist_ignore_space
 
 # 履歴からコマンド補完
-autoload history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^p" history-beginning-search-backward-end
-bindkey "^n" history-beginning-search-forward-end
+function peco-select-history() {
+    local tac
+    if which tac > /dev/null; then
+        tac="tac"
+    else
+        tac="tail -r"
+    fi
+    BUFFER=$(history -n 1 | \
+        eval $tac | \
+        peco --query "$LBUFFER")
+    CURSOR=$#BUFFER
+    zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
